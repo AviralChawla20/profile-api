3f1fed9b1c9541a29b32a52c2b0a5a28160ca5f3
AviralChawla20 <aviralchawla02@gmail.com.com>
2023-11-23 23:05:44 +0530

    fix

--- api.py
+++ api.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi_cache import FastAPICache, caches
 from sqlalchemy import create_engine, Column, BigInteger, DateTime, Text
 from sqlalchemy.orm import declarative_base, sessionmaker
+from cachetools import cached, TTLCache
+from pydantic import BaseModel
 from typing import Optional
 
 # Create a FastAPI app
@@ -16,15 +16,6 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
-
-# Configure FastAPI Cache
-FastAPICache.init(
-    app,
-    caches=[
-        {"type": "memory", "expire": 60},
-        {"type": "redis", "url": "redis://localhost:6379/1", "expire": 60},
-    ],
 )
 
 # Create a SQLAlchemy engine and session
@@ -73,18 +64,17 @@ class UserProfileUpdate(BaseModel):
     behance: Optional[str] = None
 
 
-# FastAPI endpoint for user profile
-@app.get("/profile")
-async def get_user_profile(email: str):
-    # Try to get user from cache
-    user_data = await FastAPICache.get(email)
-    if user_data is not None:
-        return user_data
+# Cache configuration
+cache = TTLCache(maxsize=100, ttl=60)  # Adjust ttl as needed
 
-    # If not in cache, fetch from the database
+
+# Cached function to get user profile
+@cached(cache)
+def get_user_profile(email: str):
+    # Fetch user from the database
     user = SessionLocal().query(User).filter_by(email=email).first()
     if user:
-        user_data = {
+        return {
             "id": user.id,
             "created_at": user.created_at,
             "name": user.name,
@@ -98,17 +88,19 @@ async def get_user_profile(email: str):
             "github": user.github,
             "behance": user.behance,
         }
-
-        # Cache the user data for future requests
-        await FastAPICache.set(email, user_data)
-        return user_data
     else:
         raise HTTPException(status_code=404, detail="User not found")
 
 
+# FastAPI endpoint for user profile
+@app.get("/profile")
+async def get_profile(email: str):
+    return get_user_profile(email)
+
+
 # FastAPI endpoint for updating user profile
 @app.post("/profile")
-async def update_user_profile(data: UserProfileUpdate):
+async def update_profile(data: UserProfileUpdate):
     user = SessionLocal().query(User).filter_by(email=data.email).first()
     if user:
         if data.name:
@@ -135,24 +127,8 @@ async def update_user_profile(data: UserProfileUpdate):
         # Commit changes to the database
         SessionLocal().commit()
 
-        # Update cached data
-        await FastAPICache.set(
-            data.email,
-            {
-                "id": user.id,
-                "created_at": user.created_at,
-                "name": user.name,
-                "email": user.email,
-                "phone_number": user.phone_number,
-                "gender": user.gender,
-                "course": user.course,
-                "specialization": user.specialization,
-                "skills": user.skills,
-                "linkedin": user.linkedin,
-                "github": user.github,
-                "behance": user.behance,
-            },
-        )
+        # Clear the cache for the updated user
+        cache.pop(data.email, None)
 
         return {"message": "Profile updated successfully"}
     else:
