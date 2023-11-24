eab4f884b1fa528192669af4ecb1b06c2ce5e046
AviralChawla20 <aviralchawla02@gmail.com.com>
2023-11-25 02:18:00 +0530

    reverted back to Flask API

--- api.py
+++ api.py
@@ -1,142 +1,86 @@
-from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy import create_engine, Column, BigInteger, DateTime, Text
-from sqlalchemy.orm import declarative_base, sessionmaker
-from cachetools import cached, TTLCache
-from pydantic import BaseModel
-from typing import Optional
-
-# Create a FastAPI app
-app = FastAPI()
-
-# Enable CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# Create a SQLAlchemy engine and session
-SQLALCHEMY_DATABASE_URI = (
-    "mysql+pymysql://sql12647981:XM51KVKzDA@sql12.freemysqlhosting.net:3306/sql12647981"
-)
-engine = create_engine(SQLALCHEMY_DATABASE_URI)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS  # Import CORS extension
 
 
-# Define the User model
-class User(Base):
+app = Flask(__name__)
+CORS(app)
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "mysql+pymysql://sql12647981:XM51KVKzDA@sql12.freemysqlhosting.net:3306/sql12647981"
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
     __tablename__ = "users"
-    id = Column(BigInteger, primary_key=True, autoincrement=True)
-    created_at = Column(DateTime)
-    updated_at = Column(DateTime)
-    deleted_at = Column(DateTime, index=True)
-    name = Column(Text(collation="latin1_swedish_ci"))
-    email = Column(Text(collation="latin1_swedish_ci"))
-    password = Column(Text(collation="latin1_swedish_ci"))
-    college_name = Column(Text(collation="latin1_swedish_ci"))
-    phone_number = Column(Text(collation="latin1_swedish_ci"))
-    gender = Column(Text(collation="latin1_swedish_ci"))
-    course = Column(Text(collation="latin1_swedish_ci"))
-    specialization = Column(Text(collation="latin1_swedish_ci"))
-    skills = Column(Text(collation="latin1_swedish_ci"))
-    linkedin = Column(Text(collation="latin1_swedish_ci"))
-    github = Column(Text(collation="latin1_swedish_ci"))
-    behance = Column(Text(collation="latin1_swedish_ci"))
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+    deleted_at = db.Column(db.DateTime, index=True)
+    name = db.Column(db.Text(collation="latin1_swedish_ci"))
+    email = db.Column(db.Text(collation="latin1_swedish_ci"))
+    password = db.Column(db.Text(collation="latin1_swedish_ci"))
+    college_name = db.Column(db.Text(collation="latin1_swedish_ci"))
+    phone_number = db.Column(db.Text(collation="latin1_swedish_ci"))
+    gender = db.Column(db.Text(collation="latin1_swedish_ci"))
+    course = db.Column(db.Text(collation="latin1_swedish_ci"))
+    specialization = db.Column(db.Text(collation="latin1_swedish_ci"))
+    skills = db.Column(db.Text(collation="latin1_swedish_ci"))
+    linkedin = db.Column(db.Text(collation="latin1_swedish_ci"))
+    github = db.Column(db.Text(collation="latin1_swedish_ci"))
+    behance = db.Column(db.Text(collation="latin1_swedish_ci"))
 
 
-# Pydantic model for request data
-class UserProfileUpdate(BaseModel):
-    email: str
-    name: Optional[str] = None
-    phone_number: Optional[str] = None
-    college_name: Optional[str] = None
-    gender: Optional[str] = None
-    course: Optional[str] = None
-    specialization: Optional[str] = None
-    skills: Optional[str] = None
-    linkedin: Optional[str] = None
-    github: Optional[str] = None
-    behance: Optional[str] = None
+@app.route("/profile", methods=["GET", "POST"])
+def user_profile():
+    if request.method == "GET":
+        user_email = request.args.get("email")
+        print("Received email:", user_email)
+        user = User.query.filter_by(email=user_email).first()
+        if user:
+            user_data = {
+                "id": user.id,
+                "created_at": user.created_at,
+                "updated_at": user.updated_at,
+                "deleted_at": user.deleted_at,
+                "name": user.name,
+                "email": user.email,
+                "password": user.password,
+                "college_name": user.college_name,
+                "phone_number": user.phone_number,
+                "gender": user.gender,
+                "course": user.course,
+                "specialization": user.specialization,
+                "skills": user.skills,
+                "linkedin": user.linkedin,
+                "github": user.github,
+                "behance": user.behance,
+            }
+            return jsonify(user_data)
+        else:
+            return jsonify({"message": "User not found"})
 
-
-# Cache configuration
-cache = TTLCache(maxsize=100, ttl=60)  # Adjust ttl as needed
-
-
-# Cached function to get user profile
-@cached(cache)
-def get_user_profile(email: str):
-    # Fetch user from the database
-    user = SessionLocal().query(User).filter_by(email=email).first()
-    if user:
-        return {
-            "id": user.id,
-            "created_at": user.created_at,
-            "name": user.name,
-            "email": user.email,
-            "phone_number": user.phone_number,
-            "college_name": user.college_name,
-            "gender": user.gender,
-            "course": user.course,
-            "specialization": user.specialization,
-            "skills": user.skills,
-            "linkedin": user.linkedin,
-            "github": user.github,
-            "behance": user.behance,
-        }
-    else:
-        raise HTTPException(status_code=404, detail="User not found")
-
-
-# FastAPI endpoint for user profile
-@app.get("/profile")
-async def get_profile(email: str):
-    return get_user_profile(email)
-
-
-# FastAPI endpoint for updating user profile
-@app.post("/profile")
-async def update_profile(data: UserProfileUpdate):
-    user = SessionLocal().query(User).filter_by(email=data.email).first()
-    if user:
-        if data.name:
-            user.name = data.name
-        if data.phone_number:
-            user.phone_number = data.phone_number
-        if data.college_name:
-            user.college_name = data.college_name
-        if data.gender:
-            user.gender = data.gender
-        if data.course:
-            user.course = data.course
-        if data.specialization:
-            user.specialization = data.specialization
-        if data.skills:
-            user.skills = data.skills
-        if data.linkedin:
-            user.linkedin = data.linkedin
-        if data.github:
-            user.github = data.github
-        if data.behance:
-            user.behance = data.behance
-
-        # Commit changes to the database
-        SessionLocal().commit()
-
-        # Clear the cache for the updated user
-        cache.pop(data.email, None)
-
-        return {"message": "Profile updated successfully"}
-    else:
-        raise HTTPException(status_code=404, detail="User not found")
+    elif request.method == "POST":
+        data = request.get_json()
+        user_email = data.get("email")
+        user = User.query.filter_by(email=user_email).first()
+        if user:
+            user.name = data.get("name")
+            user.phone_number = data.get("phone_number")
+            user.college_name = data.get("college_name")
+            user.gender = data.get("gender")
+            user.course = data.get("course")
+            user.specialization = data.get("specialization")
+            user.skills = data.get("skills")
+            user.linkedin = data.get("linkedin")
+            user.github = data.get("github")
+            user.behance = data.get("behance")
+            db.session.commit()
+            return jsonify({"message": "Profile updated successfully"})
+        else:
+            return jsonify({"message": "User not found"})
 
 
 if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
